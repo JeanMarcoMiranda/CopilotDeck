9e96aa94937576a9f7ffcb9eba0aeb7ff368a60f
JeanMarcoMiranda <51245939+JeanMarcoMiranda@users.noreply.github.com>
2024-05-13 20:04:05 -0500

    Update MainActivity.kt

--- app/src/main/java/com/mrmisti/copilot/deck/MainActivity.kt
+++ app/src/main/java/com/mrmisti/copilot/deck/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CopilotDeckTheme {
                 // A surface container using the 'background' color from the theme
-                // Cuack
+                // Cuack editado
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
